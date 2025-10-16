5b92e2ca8ebe2fe32583e1d1e2f83467e6a67d3f
Simonsanchezs <simon.sanchezs@udea.edu.co>
2025-10-16 18:23:19 -0500

    Implement vector data structure with dynamic resizing and basic operations; buid in commands created
    Co-authored-by: Spelaez122 <sebastianpz122@gmail.com>
    Co-authored-by: julianisazam <julian.isazam@udea.edu.co>

---
+++ enunciado/vector.h
@@ -0,0 +1,21 @@
+#define INITIAL_VECTOR_CAPACITY 8
+
+typedef struct Vector
+{
+    char** data;
+    int capacity;
+    int size;
+} Vector;
+
+
+Vector create_vector();
+
+void push_back(Vector*, char*);
+
+void pop_back(Vector*);
+
+char* get(Vector*, int);
+
+int search_key (Vector*, char*);
+
+void destroy (Vector*);
--- enunciado/wish.c
+++ enunciado/wish.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "vector.h"
 
-
-
+void showError ();
 
 int main (int argc, char* argv[]){
     FILE* input_file = NULL;
@@ -29,4 +29,31 @@ int main (int argc, char* argv[]){
         free(line);
     }
     
+}
+
+int execute_command (Vector tokens) {
+    char* command = get(&tokens, 0);
+    if (strcmp(command, "exit") == 0) {
+        if (tokens.size != 1) showError();
+        else exit(0);
+        return -1;
+    }
+    if (
+        strcmp(command, "cd") == 0
+    ) {
+        if (tokens.size != 2) showError();
+        else chdir(get(&tokens, 1));
+        return -1;
+    }
+    if (tokens.size >= 1 && strcmp("path", command) == 0) {
+        Vector params = create_vector();
+        for (int i = 1; i < tokens.size; i++) push_back(&params, get(&tokens, i));
+        PATH = params;
+        return -1;
+    }
+}
+
+void showError () {
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message)); 
 }
