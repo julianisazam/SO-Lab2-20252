63f7278383900a87b731b5bd26115be8a1813372
Simonsanchezs <simon.sanchezs@udea.edu.co>
2025-10-19 15:13:00 -0500

    main function finished, redirection and parallel commands created and error handling
    Co-authored-by: Spelaez122 <sebastianpz122@gmail.com>
    Co-authored-by: julianisazam <julian.isazam@udea.edu.co>

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -3,8 +3,27 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "vector.h"
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+
+Vector PATH;
+
+
+Vector parse (char* line);
+
+int isSameFile (int, int);
+int isDelimiter (char);
+int isValidRedirection (Vector tokens);
+int isValidAmpersand (Vector tokens);
+char* concat (char* a, char* b);
 
 void showError ();
+
+int execute_command (Vector tokens);
+
 
 int main (int argc, char* argv[]){
     FILE* input_file = NULL;
@@ -14,7 +33,22 @@ int main (int argc, char* argv[]){
             showError();
             exit(1);
         }
+        
+        if (input_file == NULL) {
+            input_file = cur_file;
+        } else {
+            if (!isSameFile(fileno(input_file), fileno(cur_file))) {
+                showError();
+                exit(1);
+            }
+        }
     }
+    
+    if (input_file != NULL) stdin = fdopen(fileno(input_file), "r");
+
+    push_back(&PATH, "/bin");
+
+
 
     while (1){
         if (input_file == NULL) printf("wish> ");
@@ -25,10 +59,47 @@ int main (int argc, char* argv[]){
             if (input_file == NULL) continue;
             break;
         }
+        line[len-1] = '\0';
+        Vector tokens = parse(line);
+        if (!isValidAmpersand(tokens)) {
+            continue;
+        }
+        int commandsCount = 1;
+        for (int i = 0; i < tokens.size; i++) {
+            if (strcmp("&", get(&tokens, i)) == 0) commandsCount++;
+        }
+        Vector command = create_vector();
+        int processIds[commandsCount];
+        int sz = 0;
+        for (int i = 0; i < tokens.size; i++) {
+            if (strcmp("&", get(&tokens, i)) != 0) {
+                push_back(&command, get(&tokens, i));
+            } else {
+                continue;
+            }
+            if (
+                i == tokens.size - 1 ||
+                strcmp("&", get(&tokens, i+1)) == 0
+            ) {
+                if (!isValidRedirection(command)) {
+                    showError();
+                    break;
+                }
+                int res = execute_command(command);
+                if (res != -1) processIds[sz++] = res;
+                command = create_vector();
+            }
+        }
+
+        for (int i = 0; i < sz; i++) waitpid(processIds[i], NULL, 0);
 
         free(line);
+        destroy(&tokens);
+        destroy(&command);
     }
-    
+
+return 0;
+
 }
 
 int execute_command (Vector tokens) {
@@ -51,9 +122,106 @@ int execute_command (Vector tokens) {
         PATH = params;
         return -1;
     }
+
+    int pos = search_key(&tokens, ">");
+    if (pos == -1) pos = tokens.size;
+    for (int i = 0; i < PATH.size; i++) {
+        char* p = concat(get(&PATH, i), concat("/", command));
+        if (access(p, X_OK) == 0) {
+            char* argv[pos+1];
+            for (int i = 0; i < pos; i++) argv[i] = get(&tokens, i);
+            argv[pos] = NULL;
+            
+            int rc = fork();
+            if (rc == 0) {
+                if (pos != tokens.size) {
+                    close(STDOUT_FILENO);
+                    open(
+                        get(&tokens, tokens.size - 1),
+                        O_CREAT | O_WRONLY | O_TRUNC,
+                        S_IRWXU
+                    );
+                }
+                execv(p, argv);
+            }
+            return rc;
+        }
+    }
+
+    showError();
+    return -1;
+}
+
+int isDelimiter (char c) {
+    return (c == ' ') || (c == '\t') || (c == '>') || (c == '&');
 }
 
 void showError () {
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message)); 
 }
+
+
+Vector parse (char* line) {
+    Vector ans = create_vector();
+    int n = strlen(line);
+    char* s = NULL;
+    int start = -1;
+    for (int i = 0; i < n; i++) {
+        if (line[i] == '>') {
+            push_back(&ans, ">");
+            continue;
+        }
+        if (line[i] == '&') {
+            push_back(&ans, "&");
+            continue;
+        }
+        if (!isDelimiter(line[i])) {
+            if (i == 0 || isDelimiter(line[i-1])) start = i;
+            if (i == n-1 || isDelimiter(line[i+1])) {
+                s = strndup(line+start, i-start+1);
+                push_back(&ans, s);
+            }
+        }
+    }
+    return ans;
+}
+
+int isSameFile (int fd1, int fd2) {
+    struct stat stat1, stat2;
+    if(fstat(fd1, &stat1) < 0) return -1;
+    if(fstat(fd2, &stat2) < 0) return -1;
+    return (stat1.st_dev == stat2.st_dev) && (stat1.st_ino == stat2.st_ino);
+}
+
+int isValidRedirection (Vector tokens) {
+    int n = tokens.size;
+    for (int i = 0; i < n; i++) {
+        if (strcmp(">", get(&tokens, i)) == 0) {
+            if (i == 0 || i == n-1 || n-1-i > 1) return 0;
+            if (i != n-1 && strcmp(">", get(&tokens, i+1)) == 0) return 0;
+        }
+    }
+    return 1;
+}
+
+int isValidAmpersand (Vector tokens) {
+    int n = tokens.size;
+    for (int i = 0; i < n; i++) {
+        if (strcmp("&", get(&tokens, i)) == 0) {
+            if (i == 0) return 0;
+            if (i != n-1 && strcmp("&", get(&tokens, i+1)) == 0) return 0;
+        }
+    }
+    return 1;
+}
+
+char* concat (char* a, char* b) {
+    char* ans = (char*) malloc((strlen(a) + strlen(b) + 1) * sizeof(char));
+    strcpy(ans, a);
+    strcat(ans, b);
+    return ans;
+}
+
+
+
